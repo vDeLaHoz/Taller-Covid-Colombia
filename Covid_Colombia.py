070a517683f3346fbae55cc8c2bc2c93f587c1a8
Victor De La Hoz <victordelahoz13@gmail.com>
2022-06-01 23:43:16 -0500

    Primer grupo de preguntas

--- Covid_Colombia.py
+++ Covid_Colombia.py
@@ -19,3 +19,66 @@ data.drop('Unidad de medida de edad', axis = 1, inplace=True)
 data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
 data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
 data.drop('ID de caso', axis = 1, inplace=True)
+
+
+# 1.Número de casos de Contagiados en el País
+
+casosContagiados = data.shape[0]
+print(f'Número de casos de contagiados en el pais es: {casosContagiados}')
+
+
+# 2.Número de Municipios Afectados
+
+municipiosAfectados = data['Nombre municipio'].value_counts().count()
+print(f'El número de municipios afectados es: {municipiosAfectados}')
+
+
+# 3.Liste los municipios afectados (sin repetirlos)
+
+municipiosAfectados_noRep = data['Nombre municipio'].value_counts()
+print(f'Municipios afectados: {municipiosAfectados_noRep}')
+
+
+# 4.Número de personas que se encuentran en atención en casa
+
+data.loc[data['Ubicación del caso'] == 'casa'] = 'Leve'
+data.loc[data['Ubicación del caso'] == 'CASA'] = 'Leve'
+atencionEnCasa = data[data['Ubicación del caso'] == 'Casa'].shape[0]
+print(f'El numero de personas con atención en casa es de: {atencionEnCasa}')
+
+
+# 5.Número de personas que se encuentran recuperados
+
+recuperados = data[data['Recuperado'] == 'Recuperado'].shape[0]
+print(f'Personas recuperadas: {recuperados}')
+
+
+# 6. Número de personas que ha fallecido
+
+fallecidos = data[data['Estado'] == 'Fallecido'].shape[0]
+print(f'El numero de fallecidos es: {fallecidos}')
+
+
+# 7.Ordenar de Mayor a menor por tipo de caso (Importado, en estudio, Relacionado)
+
+caso = data['Tipo de contagio' ].value_counts().head()
+print(f'Tipo de caso:\n{caso}')
+
+
+# 8.Número de departamentos afectados
+
+departamentosAfectados = data['Nombre departamento'].value_counts().count()
+print(f'Departamentos afectados: {departamentosAfectados}')
+
+
+# 9.Liste los departamentos afectados(sin repetirlos)
+
+departamentosAfectados_noRep = data['Nombre departamento'].value_counts()
+print(f'Departamentos afectados: {departamentosAfectados_noRep}')
+
+
+# 10.Ordene de mayor a menor por tipo de atención
+
+atencion = data['Tipo de recuperación'].value_counts().head()
+print('*'*50)
+print(f'Tipo de atencion:\n{atencion}')
