e074f997a08ea612d08d08378f9d5afee5af8fea
Victor De La Hoz <victordelahoz13@gmail.com>
2022-06-01 23:20:30 -0500

    Obtencion y tratamiento de la data

---
+++ Covid_Colombia.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Obtener la data
+
+url = 'Casos_positivos_de_COVID-19_en_Colombia.csv'
+data = pd.read_csv(url)[:10000]
+
+# Tratamiento de la data
+
+data.drop('Código ISO del país', axis = 1, inplace=True)
+data.drop('Fecha de muerte', axis = 1, inplace=True)
+data.drop('Nombre del país', axis = 1, inplace=True)
+data.drop('Pertenencia étnica', axis = 1, inplace=True)
+data.drop('Nombre del grupo étnico', axis = 1, inplace=True)
+data.drop('Fecha de inicio de síntomas', axis = 1, inplace=True)
+data.drop('Unidad de medida de edad', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
+data.drop('ID de caso', axis = 1, inplace=True)
